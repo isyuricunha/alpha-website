ebe81e80a11eddc646c2e6f842ea27d84249285a
Yuri Cunha <isyuricunha@duck.com>
2024-03-12 00:59:19 -0300

    Update LinkedIn URL in constants.ts

--- config/constants.ts
+++ config/constants.ts
@@ -3,7 +3,7 @@ export default {
     discord: "https://discord.gg/rpvNmR9cP9",
     twitter: "https://twitter.com/isyuricunha/",
     github: "https://github.com/isyuricunha/",
-    linkedIn: "https://linkedin.com/in/isyuricunha",
+    linkedIn: "https://linkedin.com/in/isyuricunha/",
     instagram: "https://instagram.com/isyuricunha/",
     email: "me@yuricunha.com",
   },
