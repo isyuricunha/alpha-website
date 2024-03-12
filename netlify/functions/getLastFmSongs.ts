1cfbc54477dabd19141a999d6f07465118d0f386
Yuri Cunha <isyuricunha@duck.com>
2024-03-12 13:33:26 -0300

    Update CORS origin for production environment

--- netlify/functions/getLastFmSongs.ts
+++ netlify/functions/getLastFmSongs.ts
@@ -24,7 +24,7 @@ const handler: Handler = async () => {
     // Origin for CORS
     const origin =
       process.env.NODE_ENV === "production"
-        ? "https://yuricunha.com"
+        ? "https://alpha-website-yuricunha.netlify.app"
         : "http://localhost:3000"
 
     // Map track function
