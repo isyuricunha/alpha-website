0cd407643d52de06bbe6100e04a17ca387b9fbb3
Yuri Cunha <isyuricunha@duck.com>
2024-03-12 13:34:42 -0300

    Update LastFM username in getLastFmSongs.ts

--- netlify/functions/getLastFmSongs.ts
+++ netlify/functions/getLastFmSongs.ts
@@ -3,7 +3,7 @@ import LastFMTyped from "lastfm-typed"
 
 // Can also be set through Netlify environment variables
 const LASTFM_API_KEY = process.env.LASTFM_API_KEY
-const username = "isyuricunha"
+const username = "yuricunha"
 
 const handler: Handler = async () => {
   if (!LASTFM_API_KEY)
