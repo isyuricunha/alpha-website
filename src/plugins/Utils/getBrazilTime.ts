a3d1510bbdfe1f7643dbd49c2121ac9d63f8459e
Yuri Cunha <isyuricunha@duck.com>
2024-03-14 12:20:51 -0300

    Update date formatting and time calculation for Brazil

--- src/plugins/Utils/getBrazilTime.ts
+++ src/plugins/Utils/getBrazilTime.ts
@@ -1,6 +1,6 @@
 /**
  * I don't know if I'm too dumb to get it using browser JS but every method,
- * every package I tried doesn't really return the correct date in Turkey
+ * every package I tried doesn't really return the correct date in Brazil
  * so I made a worker that returns the correct date.
  */
 export default function getBrazilTime(): Date {
